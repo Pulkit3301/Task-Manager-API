623d894de5289b20f3d0bd6bc8a5dfae0d53b658
Pulkit Varshney <pulkit.varshney@office365.3pillarglobal.com>
2021-11-14 22:56:31 +0530

    setup some custom errors, and refractores the code

--- app.js
+++ app.js
@@ -3,14 +3,21 @@ const app = express();
 const tasks = require("./routes/tasks");
 const connectDB = require("./DB/connect");
 require("dotenv").config();
-//middleware
-app.use(express.json());
+const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
+//middleware
+
+app.use(express.json());
 app.use(express.static("./public"));
 
-app.use("/api/v1/tasks", tasks);
+//routes
 
-const port = 3000;
+app.use("/api/v1/tasks", tasks);
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
