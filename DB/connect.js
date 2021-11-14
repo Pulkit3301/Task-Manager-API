11c81dd044b4c60e4c7c7c9de0ba4d5dbf0945e9
Pulkit Varshney <pulkit.varshney@office365.3pillarglobal.com>
2021-11-14 15:08:43 +0530

    refractored the code

--- DB/connect.js
+++ DB/connect.js
@@ -3,14 +3,12 @@ const mongoose = require("mongoose");
 const connectionString =
   "mongodb+srv://pulkit:pulkit2012@nodeexpressprojects.cvvaz.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority";
 
-mongoose
-  .connect(connectionString, {
+const connectDB = (url) => {
+  mongoose.connect(connectionString, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Connected to the DB");
-  })
-  .catch((err) => console.log(err));
+  });
+};
+module.exports = connectDB;
--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-require("./DB/connect");
 const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
+const connectDB = require("./DB/connect");
 //middleware
 app.use(express.json());
 
@@ -14,6 +14,15 @@ app.use("/api/v1/tasks", tasks);
 
 const port = 3000;
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}....`);
-});
+const start = async () => {
+  try {
+    await connectDB();
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}....`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
